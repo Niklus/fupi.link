771dd27fdd4de1d10c6824ce80c6eac7a2d8ae7c
Nicholas Otieno <niklasoti@gmail.com>
2023-10-09 16:01:47 +0200

    Update

--- middlewares/security.js
+++ middlewares/security.js
@@ -4,11 +4,20 @@ import { helmet } from "../deps.js";
 const security = (ctx, next) => {
   ctx.state.nonce = nanoid(32);
   return helmet({
-    crossOriginResourcePolicy: false,
+    // crossOriginResourcePolicy: false,
     contentSecurityPolicy: {
       directives: {
+        defaultSrc: ["'self'"],
         scriptSrc: ["'self'", `'nonce-${ctx.state.nonce}'`],
         styleSrc: ["'self'", `'nonce-${ctx.state.nonce}'`],
+        scriptSrcElem: [
+          "'https://cdnjs.cloudflare.com'",
+          `'nonce-${ctx.state.nonce}'`,
+        ],
+        styleSrcElem: [
+          "'https://cdn.jsdelivr.net'",
+          `'nonce-${ctx.state.nonce}'`,
+        ],
       },
     },
   })(ctx, next);
