2d9c96c3386977187e202c1037a1730fc18f1ffe
Nicholas Otieno <niklasoti@gmail.com>
2023-10-09 15:31:24 +0200

    Update

--- middlewares/security.js
+++ middlewares/security.js
@@ -6,16 +6,8 @@ const security = (ctx, next) => {
   return helmet({
     contentSecurityPolicy: {
       directives: {
-        scriptSrc: [
-          "'self'",
-          `'nonce-${ctx.state.nonce}'`,
-          "*cdnjs.cloudflare.com",
-        ],
-        styleSrc: [
-          "'self'",
-          `'nonce-${ctx.state.nonce}'`,
-          "*.cdn.jsdelivr.net",
-        ],
+        scriptSrc: [`'nonce-${ctx.state.nonce}'`, "*cdnjs.cloudflare.com"],
+        styleSrc: [`'nonce-${ctx.state.nonce}'`, "*.cdn.jsdelivr.net"],
       },
     },
   })(ctx, next);
